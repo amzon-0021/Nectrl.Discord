06bfa68a99df44733ba0b32eebdf2df09bb68b7f
amzon-0021 <58093149+amzon-0021@users.noreply.github.com>
2020-05-22 20:02:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<?php
-	http_response_code( 301 ) ;
-
-	header( "Location: https://syncer.jp/" ) ;
-	exit ;
+<html>
+	<body>
+		<h1>ようこそ!Nectrl連携は、以下のURLから行えます</h1>
+		<p href="https://discord.com/oauth2/authorize?client_id=690811499215716384&permissions=8&redirect_uri=https%3A%2F%2Famzon-0021.github.io%2FNectrl.Discord%2Fbot.html&response_type=code&scope=guilds.join%20connections%20guilds%20gdm.join%20email%20identify%20rpc%20rpc.notifications.read%20bot%20webhook.incoming%20applications.builds.upload%20messages.read%20applications.builds.read%20applications.store.update%20applications.entitlements%20activities.read%20activities.write%20relationships.read" target="_blank" rel="noopener">ここからどうぞ</p>
+	</body>
+	
+</html>
